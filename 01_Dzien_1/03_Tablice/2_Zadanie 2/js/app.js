8b497fd32497169dd445009f6b777088264850eb
wojtek0126 <wojtek0126@gmail.com>
2020-08-29 15:27:49 +0200

    zadania JS dzien1 druga czesc

--- 01_Dzien_1/03_Tablice/2_Zadanie 2/js/app.js
+++ 01_Dzien_1/03_Tablice/2_Zadanie 2/js/app.js
@@ -1,1 +1,21 @@
+// Stwórz tablicę z listą swoich ulubionych owoców. Następnie:
 
+// 1. Wypisz pierwszy owoc w konsoli. v
+// 1. Wypisz ostatni owoc w konsoli (skorzystaj z atrybutu ```length```).v
+// 1. W pętli wypisz wszystkie owoce (skorzystaj z atrybutu ```length```).
+
+fruits = ["malina", "borówka", "agrest", "kujowy_kokos"];
+
+console.log(fruits[0]);
+
+// console.log(fruits.slice(-1)[0]);
+
+function last(array) {
+    return array[array.length - 1];
+}
+
+console.log(last(fruits));
+// zrob wersje z length ponizej tylko wersja na szybko
+fruits.forEach(function(item, index) {
+    console.log('index ' + index + ': ' + item);
+});
