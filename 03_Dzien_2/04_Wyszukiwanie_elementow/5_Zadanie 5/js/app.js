d7df7a6cb89f59bbdbef2cbd4c07c7813812b57a
wojtek0126 <wojtek0126@gmail.com>
2020-09-09 16:32:51 +0200

    zadania z drugiego dnia - DOM

--- 03_Dzien_2/04_Wyszukiwanie_elementow/5_Zadanie 5/js/app.js
+++ 03_Dzien_2/04_Wyszukiwanie_elementow/5_Zadanie 5/js/app.js
@@ -1,1 +1,11 @@
+// 1. Wyszukaj na stronie i zapisz do odpowiednio nazwanej zmiennej **tag** ```article``` o **klasie** ```first```.
+// 2. W kolejnym etapie:
+//     * wypisz w konsoli, ile elementów **h2** znajduje się w tym **tagu**.
+//
+// Pamiętaj, żeby za każdym razem sprawdzić, czy wczytałeś odpowiednie elementy.
 
+const firstArticle = document.querySelector('article.first');
+
+console.log(firstArticle.querySelectorAll('h2'));
+console.log("W tym tagu znajdują się " + firstArticle.querySelectorAll('h2').length +
+" elementy h2");
