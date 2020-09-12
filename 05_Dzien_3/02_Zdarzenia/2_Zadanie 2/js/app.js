bdea8d34a96ad2ed5bf19a4b0188a9dc4eed72d3
wojtek0126 <wojtek0126@gmail.com>
2020-09-12 16:10:55 +0200

    Zadanie 2 czyli DOMcontentLoaded lub defer/async

--- 05_Dzien_3/02_Zdarzenia/2_Zadanie 2/js/app.js
+++ 05_Dzien_3/02_Zdarzenia/2_Zadanie 2/js/app.js
@@ -1,5 +1,18 @@
-const menu = document.querySelector("#menu");
-const paragraf = document.querySelector("p");
+// Uruchom stronę przygotowaną dla zadania. Sprawdź w konsoli, czy są jakieś błędy. Następnie:
+// 1. Przenieś tag ```<script>``` do sekcji ```<head>```. Zobacz, co się zmieniło w działaniu skryptu.
+//     Spróbuj to wytłumaczyć.
+// 2. Stwórz event ```DOMContentLoaded``` dla elementu ```document``` i przenieś kod do środka tego eventu.
+//     Czy poprawiło to sytuację?
+//     3. Przenieś znowu tag ```<script>``` na koniec pliku &ndash; czy jest jakaś różnica?
 
-menu.classList.add("menu");
-paragraf.innerHTML = "A to jest paragraf w zadaniu 2";
+document.addEventListener("DOMContentLoaded", function () {  //dziala jak async defer - mozna odpalac skrypta z head
+    console.log("DOM fully loaded and parsed!");
+    const menu = document.querySelector("#menu");
+    const paragraf = document.querySelector("p");
+
+    menu.classList.add("menu");
+    paragraf.innerHTML = "A to jest paragraf w zadaniu 2";
+});
+
+
+
