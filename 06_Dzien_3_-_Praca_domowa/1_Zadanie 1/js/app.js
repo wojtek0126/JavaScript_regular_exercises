f89177711959709f5a81bb0b18fe278020b79103
wojtek0126 <wojtek0126@gmail.com>
2020-09-13 09:45:14 +0200

    eventy praca domowa dzien 3 toggle classes on click/hover

--- 06_Dzien_3_-_Praca_domowa/1_Zadanie 1/js/app.js
+++ 06_Dzien_3_-_Praca_domowa/1_Zadanie 1/js/app.js
@@ -12,7 +12,10 @@
 //
 const articleButtons = document.querySelectorAll('.btn');
 const invisibleDivs = document.querySelectorAll('.content');
-
+// const
+// petla forEach duzo lepsza niz moje rozwiazanie, bo jak ktos cos zmieni w HTML, to sie wykrzaczy i bedzie babol!!!
+// w tym wypadku mozna tez dodac klasy do elementow zeby odrozniac ktore maja dostac ktore eventy(tu hover i click),
+// i wtedy forEach.
 const invDiv1 = invisibleDivs[0];
 const invDiv2 = invisibleDivs[1];
 const btn1 = articleButtons[0];
@@ -20,20 +23,33 @@ const btn2 = articleButtons[1];
 console.log(btn1);
 console.log(btn2);
 
-function showContentOnClick(){
+function showContentOnClick(event){
+    event.preventDefault();
     invDiv1.classList.toggle('d-none');
+    if (this.innerText == "Czytaj więcej") { //dwa znaki ==, jak bedzie jeden = to nie zadziala wtedy  to przypisanie
+        this.innerText = "Ukryj";
+    }
+    else {
+        this.innerText = "Czytaj więcej";
+    }
 }
 
-function showContentOnHover(){
+function showContentOnHover(event){
+    event.preventDefault();   // dziala, ma powstrzymac # po nacisnieciu w link
     invDiv2.classList.toggle('d-none');
 }
 
-btn1.addEventListener("click", showContentOnClick);
+// btn1.addEventListener("click", showContentOnClick);
 btn2.addEventListener("mouseover", showContentOnHover);
 btn2.addEventListener("mouseout", showContentOnHover);
 
 
-
+// funkcja do eventu - do lib na gita;type np.'click' --->dziala, tylko po co?
+function events(element, type, func) {
+    element.addEventListener(type, func);
+}
+//wywolanie
+events(btn1, 'click', showContentOnClick);
 
 // //czy mozna zbudowac uniwersalna funkcje template do eventow, pseudokod ponizej, byc moze mix jquery i JS, ma to sens?
 // function eventForSingleElement(elementMain--->element_do_obrobki, eventName--->np onClick, action-->np.
