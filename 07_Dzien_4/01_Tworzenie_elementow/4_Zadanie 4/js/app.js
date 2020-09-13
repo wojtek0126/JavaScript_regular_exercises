d5b8b914a65e7cf980a797f318caaea4d57bdf28
wojtek0126 <wojtek0126@gmail.com>
2020-09-13 12:53:30 +0200

    niekompletne teraz zadanie 4 formularz dodaj wpisy

---
+++ 07_Dzien_4/01_Tworzenie_elementow/4_Zadanie 4/js/app.js
@@ -0,0 +1,8 @@
+// Na stronie znajduje się tabela z informacjami na temat zamówień (z dwoma już wprowadzonymi zamówieniami).
+// Poniżej znajduje się formularz do wypełnienia nowego zamówienia. Napisz event, który pobierze informacje z inputów
+// (```el.value```) i wprowadzi nowy wpis do tabeli.
+
+const $orderId = document.getElementById('orderId');
+const $orderId = document.getElementById('item');
+const $orderId = document.getElementById('quantity');
+// cdn
