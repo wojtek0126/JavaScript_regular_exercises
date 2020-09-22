032ef6c5fa18aa1069b51b350566cde8d525ed20
wojtek0126 <wojtek0126@gmail.com>
2020-09-22 20:10:04 +0200

    zadanie domowe formulaż cd...

--- 08_Dzien_4_-_Praca_domowa/1_Zadanie 1/js/app.js
+++ 08_Dzien_4_-_Praca_domowa/1_Zadanie 1/js/app.js
@@ -16,46 +16,49 @@
 
 
 const checkboxes = document.querySelectorAll('.form-check-input');
-console.log(checkboxes);
 
 const $extraCheeseCheckbox = document.getElementById('cheese');
 const $extraHamCheckbox = document.getElementById('ham');
 const $extraSauceCheckbox = document.getElementById('sauce');
 const $extraPineappleCheckbox = document.getElementById('pineapple');
 const $extraMushroomCheckbox = document.getElementById('mushrooms');
-console.log($extraCheeseCheckbox, $extraHamCheckbox, $extraSauceCheckbox, $extraPineappleCheckbox, $extraMushroomCheckbox);
 
 const $extrasValue = document.getElementById('price');
-console.log($extrasValue);
 
 const orderInfo = document.querySelector('.order-info');
-console.log(orderInfo);
 
 const addAllExtrasBtn = document.getElementById('all_extras_btn');
 const removeAllExtrasBtn = document.getElementById('clear_extras_btn');
 const submitBtn = document.getElementById('sub_btn');
-console.log(addAllExtrasBtn, removeAllExtrasBtn, submitBtn);
+
+const PizzaPrices = {
+    small: 30,
+    medium: 35,
+    large: 40,
+
+    extraCheese: 3.5,
+    extraHam: 5,
+    extraSauce: 2.2,
+    extraMushrooms: 3.5,
+    extraPineapple: 4.1
+}
+
+const extraCheesePrice = PizzaPrices.extraCheese;
+const extraHAmPrice = PizzaPrices.extraHam;
+const extraSaucePrice = PizzaPrices.extraSauce;
+const extraPineapplePrice = PizzaPrices.extraPineapple;
+const extraMushroomPrice = PizzaPrices.extraMushrooms;
 
 function formatPrice(price) {
     return price.toFixed(2).replace('.', ',');
 }
-
-let prices = [3.50, 2.20, 5.00, 4.10, 3.50];
-// let extras = [cheese, ham, sauce, pineapple, mushroom];
-
-// extras.forEach(function (e) {
-//
-//
-// })
-
-
-const extraCheesePrice = prices[0];
-const extraHAmPrice = prices[2];
-const extraSaucePrice = prices[1];
-const extraPineapplePrice = prices[3];
-const extraMushroomPrice = prices[4];
-console.log(extraPineapplePrice, extraHAmPrice, extraSaucePrice, extraCheesePrice, extraMushroomPrice);
-
+function mediumPlainPizzaPrice() {
+   return PizzaPrices.medium;
+}
+function  allIExtraIngredientsPrice() {
+    let total = (PizzaPrices.extraCheese + PizzaPrices.extraHam + PizzaPrices.extraMushrooms + PizzaPrices.extraSauce + PizzaPrices.extraPineapple);
+    return total;
+}
 
 
 
@@ -63,46 +66,87 @@ checkboxes.forEach(function (e) {
     e.checked = false;
 });
 let price = 0;
-let basePrice = 35;
-let totalFull = price + basePrice;
-//zobacz czy tu sie nie da sformatowac
-function addExtra(checkbox, ingredientPrice) {
-    checkbox.addEventListener( 'click', function () {
-
-        if (checkbox.checked === true){
-            let priceAdded = price += ingredientPrice;
-            const addFinal = formatPrice(priceAdded);
-            let wholeCostUp = basePrice += ingredientPrice;
-            let addFinalFormatted = formatPrice(wholeCostUp);
-            $extrasValue.innerHTML = `${addFinal} zł`;
-            orderInfo.innerHTML = `${addFinalFormatted} zł`
-            console.log(addFinal);
-            console.log(wholeCostUp);
-
-        } else {
-            let priceDeducted = price -= ingredientPrice;
-            const reduceFinal = formatPrice(priceDeducted);
-            let wholeCostDown = basePrice -= ingredientPrice;
-            let reduceFinalFormatted = formatPrice(wholeCostDown)
-            $extrasValue.innerHTML = `${reduceFinal} zł`;
-            orderInfo.innerHTML = `${reduceFinalFormatted} zł`
-            console.log(reduceFinal);
-            console.log(wholeCostDown);
-
-        }
-
+let plainPrice = mediumPlainPizzaPrice();
+let allExtrasPrice = allIExtraIngredientsPrice();
+let fullLoadedPrice = plainPrice + allExtrasPrice;
+function allCheckboxesEnabled() {
+    return checkboxes.forEach(function (element) {
+        element.checked = true;
     })
 }
 
-addExtra($extraCheeseCheckbox, extraCheesePrice );
-addExtra($extraHamCheckbox, extraHAmPrice );
-addExtra($extraPineappleCheckbox, extraPineapplePrice );
-addExtra($extraSauceCheckbox, extraSaucePrice );
-addExtra($extraMushroomCheckbox, extraMushroomPrice );
+function addRemoveSingleExtraIngredient(checkbox, ingredientPrice) {
+    checkbox.addEventListener( 'click', function () {
+
+        if (checkbox.checked === true) {
+            let priceAdded = price += ingredientPrice;
+            const addFinal = formatPrice(priceAdded);
+            let wholeCostUp = plainPrice += ingredientPrice;
+            let addFinalFormatted = formatPrice(wholeCostUp);
+            $extrasValue.innerHTML = `${addFinal} zł`;
+            orderInfo.innerHTML = `Cała kwota: ${addFinalFormatted} zł`
+        }
+        if (checkbox.checked === false) {
+            // price = 0;
+            let priceDeducted = price -= ingredientPrice;
+            console.log(ingredientPrice);
+            const reduceFinal = formatPrice(priceDeducted);
+            let wholeCostDown = plainPrice -= ingredientPrice;
+            let reduceFinalFormatted = formatPrice(wholeCostDown)
+            $extrasValue.innerHTML = `${reduceFinal} zł`;
+            orderInfo.innerHTML = `Cała kwota: ${reduceFinalFormatted} zł`
+        }
+    })
+}
+
+addRemoveSingleExtraIngredient($extraCheeseCheckbox, extraCheesePrice );
+addRemoveSingleExtraIngredient($extraHamCheckbox, extraHAmPrice );
+addRemoveSingleExtraIngredient($extraPineappleCheckbox, extraPineapplePrice );
+addRemoveSingleExtraIngredient($extraSauceCheckbox, extraSaucePrice );
+addRemoveSingleExtraIngredient($extraMushroomCheckbox, extraMushroomPrice );
 
 
+addAllExtrasBtn.addEventListener('click', function () {
+
+    checkboxes.forEach(function (element) {
+        element.checked = true;
+    })
 
 
+    allExtras = allIExtraIngredientsPrice();
+    console.log(allExtras);
+    plainPrice = mediumPlainPizzaPrice();
+    console.log(plainPrice);
+    fullPrice = plainPrice + allExtras;
+    console.log(fullPrice);
+
+    let allExtrasFormatted = formatPrice(allExtras);
+    let plainAndExtrasFormatted = formatPrice(fullPrice);
+
+    $extrasValue.innerHTML = `${allExtrasFormatted} zł`;
+    orderInfo.innerHTML = `Cała kwota: ${plainAndExtrasFormatted} zł`;
+    price = allExtras;
+    plainPrice = fullPrice;
+    console.log(price, plainPrice, 'extras and whole after add all pressed');
+})
+
+removeAllExtrasBtn.addEventListener('click', function () {
+    checkboxes.forEach(function (element) {
+        element.checked = false;
+    })
+    price = 0;
+    plainPrice = mediumPlainPizzaPrice();
+    plainPriceFormatted = formatPrice(plainPrice);
+    priceOfNoExtrasFormatted = formatPrice(price);
+    console.log('remall');
+    $extrasValue.innerHTML = `${priceOfNoExtrasFormatted} zł`;
+    orderInfo.innerHTML = `Cała kwota: ${plainPriceFormatted} zł`
+})
+
+submitBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log('sub');
+})
 
 
 
