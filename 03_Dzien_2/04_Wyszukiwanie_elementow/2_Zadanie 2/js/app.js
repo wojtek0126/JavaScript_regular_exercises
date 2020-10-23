ef72011366f30a4057e3d5a3a565677b2185d1af
wojtek0126 <wojtek0126@gmail.com>
2020-10-23 21:18:54 +0200

    elementy, href

--- 03_Dzien_2/04_Wyszukiwanie_elementow/2_Zadanie 2/js/app.js
+++ 03_Dzien_2/04_Wyszukiwanie_elementow/2_Zadanie 2/js/app.js
@@ -1,3 +1,5 @@
-document.querySelectorAll('div'); // wychodzi mi 22
+const x = document.querySelectorAll('div'); // wychodzi mi 20
+console.log(x)
+
 document.querySelectorAll('.offers'); //12
 document.querySelectorAll('[href]'); //16
