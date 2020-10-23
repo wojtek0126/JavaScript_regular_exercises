6a784b30765f0a68b229f317069b0de697e57cce
wojtek0126 <wojtek0126@gmail.com>
2020-10-23 15:45:58 +0200

    Zadanie 8 porównywanie dwoch zbiorow licz funkcja

--- 01_Dzien_1/03_Tablice/8_Zadanie 8/js/app.js
+++ 01_Dzien_1/03_Tablice/8_Zadanie 8/js/app.js
@@ -1,1 +1,18 @@
 // sprawdz ktora dluzsza i uzyj foreach lub map.
+function addArrays(tab1, tab2) {
+    let tabMin = tab1;
+    let tabMax = tab2;
+    if (tab1.length > tab2.length) {
+        tabMin = tab2;
+        tabMax = tab1;
+    }
+    return tabMax.map(function (elm, index) {
+        if (tabMin[index]) {
+            return elm + tabMin[index];
+        } else {
+            return elm;
+        }
+    });
+}
+const wynik = addArrays([4,0,1,3,4], [1,9,6,7,8,17,23,40]);
+console.log(wynik);
